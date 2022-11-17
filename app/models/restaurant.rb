888dbc6b1d6e41ec6d6fabc8ec74eb36838e8432
Lydiasou <74137307+Lydiasou@users.noreply.github.com>
2022-11-17 19:04:12 +0100

    validations model restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ['Gourmand', 'Healthy', 'Asiatique', 'Italien', 'Cuisine du monde', 'Traditionel',
+              'Festive', 'Chic', 'Populaire', 'Fast-food', 'Casher', 'Halal', 'Vegan/Végétarien']
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: Restaurant::CATEGORY }, presence: true
 end
